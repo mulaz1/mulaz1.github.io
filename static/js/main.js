99e01cac407354fa649824b1ce114b1af4a5dd8d
Simonemulazzi <38154302+mulaz1@users.noreply.github.com>
2020-11-02 01:34:35 +0100

    fixed menu

--- static/js/main.js
+++ static/js/main.js
@@ -104,6 +104,10 @@ window.onscroll = function() {
 } 
 
 //skills
-var ProgressBar = require('progressbar.js');
+
+//var ProgressBar = require('progressbar.js');
+
+// Assuming we have an empty <div id="container"></div> in
+// HTML
 var bar = new ProgressBar.Line('#container', {easing: 'easeInOut'});
-bar.animate(0.4);  // Value from 0.0 to 1.0
+bar.animate(1);  // Value from 0.0 to 1.0
